acaab92c5f9c320d85d39260366d0d2cc895ed8b
makoto_win8 <ishikawm@fsi.co.jp>
2017-05-25 17:02:43 +0900

    静的メソッドクラス

--- RestExportApp/FormMain.cs
+++ RestExportApp/FormMain.cs
@@ -1,77 +1,53 @@
 ﻿using System;
-using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ExtensionsLibrary.Extensions;
-using NetLibrary.Extensions;
 using ObjectAnalysisProject.Extensions;
-using RestExportApp.Model;
-using WindowsFormsLibrary.Extensions;
 using OfficeOpenXml;
-using System.IO;
 using OfficeOpenXml.Table;
+using RestExportApp.Properties;
+using WindowsFormsLibrary.Extensions;
+using static RestExportApp.RestApi.YahooApi;
 
 namespace RestExportApp {
 	public partial class FormMain : Form {
-		#region フィールド
+		#region コンストラクタ
 
-		private string _appid = "dj0zaiZpPTAxaDBCNkxtd2hVTSZzPWNvbnN1bWVyc2VjcmV0Jng9ZTM-";
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		public FormMain() {
+			this.InitializeComponent();
+
+			this.AppId = Settings.Default.AppId;
+		}
 
 		#endregion
 
-		#region コンストラクタ
+		#region プロパティ
 
-		public FormMain() {
-			this.InitializeComponent();
-		}
+		/// <summary>
+		/// アプリケーションID
+		/// </summary>
+		protected string AppId { get; set; }
 
 		#endregion
 
 		#region イベントハンドラ
 
 		private async void btnGet_Click(object sender, EventArgs e) {
-			var rdo = this.group01.Controls.OfType<RadioButton>().SingleOrDefault(r => r.Checked);
-			var st = rdo?.Text;
-			this.grid01.Tag = st;
-
-			switch (st) {
-			case "降水量":
-				await this.Precipitation();
-				break;
-			case "商品検索":
-				await this.ItemSearch();
-				break;
-			case "カテゴリー":
-				await this.CategorySearch();
-				break;
+			try {
+				await GetAsync();
+			} catch (Exception ex) {
+				this.ShowErrorMessage(ex, true);
 			}
 		}
 
 		private async void btnExport_Click(object sender, EventArgs e) {
 			try {
-				var dlg = new SaveFileDialog {
-					FilterIndex = 1,
-					Title = "エクスポート",
-					Filter = "Excelファイル (*.xlsx, *.xlsm)|*.xlsx;*.xlsm",
-				};
-
-				if (dlg.ShowDialog() != DialogResult.OK) {
-					return;
-				}
-
-				var table = this.grid01.ToDataTable();
-				var name = $"{this.grid01.Tag}";
-				table.TableName = name;
-				await Task.Run(() => {
-					var file = new FileInfo(dlg.FileName);
-					using (var xlsx = new ExcelPackage(file)) {
-						var sheet = xlsx.Workbook.Worksheets.Add(name);
-						sheet.Cells["A1"].LoadFromDataTable(table, true, TableStyles.Light6);
-
-						xlsx.Save();
-					}
-				});
+				await ExportAsync();
 			} catch (Exception ex) {
 				this.ShowErrorMessage(ex, true);
 			}
@@ -81,61 +57,82 @@ namespace RestExportApp {
 
 		#region メソッド
 
-		private async Task Precipitation() {
-			var lat = 139.732293;
-			var lon = 35.663613;
+		#region GetAsync
 
-			this.ShowMessageBox($"降水量を調べます。緯度 : {lat}, 経度 : {lon}");
-			var url = $@"https://map.yahooapis.jp/weather/V1/place";
-			var para = $@"coordinates={lat},{lon}&output=json&appid={_appid}";
-			var uri = new Uri($@"{url}?{para}");
+		/// <summary>
+		/// GET 処理
+		/// </summary>
+		protected virtual async Task GetAsync() {
+			var rdo = this.group01.Controls.OfType<RadioButton>().SingleOrDefault(r => r.Checked);
+			var st = rdo?.Text;
+			this.grid01.Tag = st;
 
-			var json = await uri.GetJsonAsync<PrecipitationModel>();
-			var weathers = json.Feature.FirstOrDefault()?.Property?.WeatherList?.Weather;
-			this.grid01.DataSource = weathers.ToDataTable();
+			switch (st) {
+			case "降水量":
+				this.ShowMessageBox($"降水量を調べます。");
+				this.grid01.DataSource = await GetPrecipitationAsync(this.AppId, 139.732293, 35.663613);
+				break;
+			case "商品検索":
+				this.ShowMessageBox($"商品検索を調べます。");
+				this.grid01.DataSource = await GetItemSearchAsync(this.AppId, 635, "-sold");
+				break;
+			case "カテゴリー":
+				this.ShowMessageBox($"カテゴリーを調べます。");
+				this.grid01.DataSource = await GetCategorySearchAsync(this.AppId, 1);
+				break;
+			}
 		}
 
-		private async Task CategorySearch() {
-			this.ShowMessageBox($"カテゴリーを調べます。");
-			var url = $@"https://shopping.yahooapis.jp/ShoppingWebService/V1/json/categorySearch";
-			var category_id = 1;
-			var para = $@"category_id={category_id}&appid={_appid}";
-			var uri = new Uri($@"{url}?{para}");
+		#endregion
 
-			var jobj = await uri.GetObjectAsync();
-			var jstr = jobj.ToString();
+		#region ExportAsync
 
-			var result = jobj["ResultSet"]["0"]["Result"];
-			var children = result["Categories"]["Children"];
+		/// <summary>
+		/// エクスポート処理
+		/// </summary>
+		protected virtual async Task ExportAsync() {
+			var name = $"{this.grid01.Tag}";
 
-			var cs = children.Select(c => c.First()).Where(c => c.HasValues).Select(c => new {
-				Order = c["_attributes"]["sortOrder"].ToString(),
-				Id = c["Id"].ToString(),
-				Url = c["Url"].ToString(),
-				Title = c["Title"]["Long"].ToString(),
-			}).ToList();
-			this.grid01.DataSource = cs.ToDataTable();
+			var dlg = new SaveFileDialog {
+				// はじめのファイル名を指定する
+				FileName = $"{name}.xlsx",
+
+				// はじめに表示されるフォルダを指定する
+				InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory),
+
+				// [ファイルの種類]に表示される選択肢を指定する
+				Filter = "Excelファイル (*.xlsx, *.xlsm)|*.xlsx;*.xlsm|すべてのファイル (*.*)|*.*",
+
+				// [ファイルの種類]ではじめに選択されるものを指定する
+				FilterIndex = 1,
+
+				// タイトルを設定する
+				Title = "エクスポートのファイルを選択してください",
+			};
+
+			if (dlg.ShowDialog() != DialogResult.OK) {
+				return;
+			}
+
+			var table = this.grid01.ToDataTable();
+			table.TableName = name;
+
+			await Task.Run(() => {
+				var file = new FileInfo(dlg.FileName);
+				if (file.Exists) {
+					file.Delete();
+				}
+
+				using (var xlsx = new ExcelPackage(file)) {
+					var sheet = xlsx.Workbook.Worksheets.Add(name);
+					sheet.Cells["A1"].LoadFromDataTable(table, true, TableStyles.Light6);
+
+					xlsx.Save();
+				}
+			});
 		}
 
-		private async Task ItemSearch() {
-			this.ShowMessageBox($"商品検索を調べます。");
-			var url = $@"https://shopping.yahooapis.jp/ShoppingWebService/V1/json/itemSearch";
-			var category_id = 635;
-			var sort = "-sold";
-			var para = $@"category_id={category_id}&sort={sort}&appid={_appid}";
-			var uri = new Uri($@"{url}?{para}");
-
-			var jobj = await uri.GetObjectAsync();
-			var jstr = jobj.ToString();
-
-			var result = jobj["ResultSet"]["0"]["Result"];
-
-			var cs = result.Select(c => c.First()).Where(c => c.HasValues).Select(c => new {
-				Name = c["Name"]?.ToString(),
-				Url = c["Url"]?.ToString(),
-			}).Where(c => c.Name != null).ToList();
-			this.grid01.DataSource = cs.ToDataTable();
-		}
+		#endregion
 
 		#endregion
 	}
---
+++ RestExportApp/RestApi/YahooApi.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using ExtensionsLibrary.Extensions;
+using NetLibrary.Extensions;
+using ObjectAnalysisProject.Extensions;
+using RestExportApp.Model;
+using WindowsFormsLibrary.Extensions;
+
+namespace RestExportApp.RestApi {
+	public static partial class YahooApi {
+		#region メソッド
+
+		/// <summary>
+		/// 降水量を取得する REST API です。
+		/// </summary>
+		/// <param name="appId">アプリケーションID</param>
+		/// <param name="lat">緯度</param>
+		/// <param name="lon">経度</param>
+		/// <returns>降水量情報のテーブルを返します。</returns>
+		public static async Task<DataTable> GetPrecipitationAsync(string appId, double lat, double lon) {
+			var url = $@"https://map.yahooapis.jp/weather/V1/place";
+			var para = $@"coordinates={lat},{lon}&output=json&appid={appId}";
+			var uri = new Uri($@"{url}?{para}");
+
+			var json = await uri.GetJsonAsync<PrecipitationModel>();
+			var weathers = json.Feature.FirstOrDefault()?.Property?.WeatherList?.Weather;
+			var tbl = weathers.ToDataTable();
+			return tbl;
+		}
+
+		/// <summary>
+		/// 商品検索を取得する REST API です。
+		/// </summary>
+		/// <param name="appId">アプリケーションID</param>
+		/// <param name="category_id">カテゴリーID</param>
+		/// <param name="sort">ソート方法を表す文字列</param>
+		/// <returns>商品検索情報のテーブルを返します。</returns>
+		public static async Task<DataTable> GetItemSearchAsync(string appId, int category_id, string sort) {
+			var url = $@"https://shopping.yahooapis.jp/ShoppingWebService/V1/json/itemSearch";
+			var para = $@"category_id={category_id}&sort={sort}&appid={appId}";
+			var uri = new Uri($@"{url}?{para}");
+
+			var jobj = await uri.GetObjectAsync();
+			var jstr = jobj.ToString();
+
+			var result = jobj["ResultSet"]["0"]["Result"];
+
+			var cs = result.Select(c => c.First()).Where(c => c.HasValues).Select(c => new {
+				Name = c["Name"]?.ToString(),
+				Url = c["Url"]?.ToString(),
+			}).Where(c => c.Name != null).ToList();
+			var tbl = cs.ToDataTable();
+			return tbl;
+		}
+
+		/// <summary>
+		/// カテゴリーを取得する REST API です。
+		/// </summary>
+		/// <param name="appId">アプリケーションID</param>
+		/// <param name="category_id">カテゴリーID</param>
+		/// <returns>カテゴリー情報のテーブルを返します。</returns>
+		public static async Task<DataTable> GetCategorySearchAsync(string appId, int category_id) {
+			var url = $@"https://shopping.yahooapis.jp/ShoppingWebService/V1/json/categorySearch";
+			var para = $@"category_id={category_id}&appid={appId}";
+			var uri = new Uri($@"{url}?{para}");
+
+			var jobj = await uri.GetObjectAsync();
+			var jstr = jobj.ToString();
+
+			var result = jobj["ResultSet"]["0"]["Result"];
+			var children = result["Categories"]["Children"];
+
+			var cs = children.Select(c => c.First()).Where(c => c.HasValues).Select(c => new {
+				Order = c["_attributes"]["sortOrder"].ToString(),
+				Id = c["Id"].ToString(),
+				Url = c["Url"].ToString(),
+				Title = c["Title"]["Long"].ToString(),
+			}).ToList();
+			var tbl = cs.ToDataTable();
+			return tbl;
+		}
+
+		#endregion
+	}
+}
